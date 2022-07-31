905b2c681ec7fc89ba179e56c89e5e80c312c4f0
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-31 15:38:23 -0400

    first intent explication 3

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 // traversing the dom
 
 const btns = document.querySelectorAll('.question-btn');
-console.log(btns)
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(e){
-        console.log(e.currentTarget.parentElement.parentElement);
-    }) // con esto nos aseguramos de que tarjeta de pregunta estamos seleccionando para luego aplicar .toggle para agregar la class de css
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
+    });
 })
